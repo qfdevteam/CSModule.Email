747865d86052f3957d69072fe697b0d412ff81ff
Pavel Mikheev <donotcodeit@gmail.com>
2024-02-18 12:42:58 +0700

    Add testing core project as nuget package

--- test/CSModule.Email.EmailSender.Tests/GlobalUsings.cs
+++ test/CSModule.Email.EmailSender.Tests/GlobalUsings.cs
@@ -1,6 +1,9 @@
 global using System.Net;
-global using FluentAssertions;
 global using NUnit.Framework;
 
+global using AutoFixture;
+global using FluentAssertions;
+global using Moq;
 global using CSModule.Test;
+
 global using CSModule.Email.EmailSender.Tests.TestData;
